081cf0ffedc3857a362af4b72f7f8f6d27254935
Alexander Walford <74402359+alexanderwalford-official@users.noreply.github.com>
2022-08-22 23:31:23 +0100

    Basic Functionality Of Shopping App

--- ShoppingApp.c
+++ ShoppingApp.c
@@ -6,11 +6,18 @@ struct Customer {
     int age;
 };
 
+char* basket[];
+int basketpricevalues[];
+const char *items[10][10] = {{"Milk", "Cheese", "Chocolate Bar", "250G Sugar", "Red Apple", "Tea Bags"}, {"24", "32", "52", "42", "87", "64"}};
+struct Customer c1;
+
 void browse() {
-    // example of a multi-dimensional string array, containing our item names and stock
-    // (notice the *) 10 is the length of the char array value, not the 
-    // length of the entire array
-    const char *items[10][10] = {{"Milk", "Cheese", "Chocolate Bar", "250G Sugar", "Red Apple", "Tea Bags"}, {"24", "32", "52", "42", "87", "64"}};
+    /* 
+        Example of a multi-dimensional string array, containing our item names
+        and stock (notice the *) 10 is the length of the char array value, not 
+        the length of the entire array. 
+    */
+
     printf("\n");
     printf("Items Currently In Stock: \n");
     printf("NAME            STOCK\n");
@@ -20,17 +27,53 @@ void browse() {
 }
 
 void basket () {
-    
+    int i; // localised pointer
+    int total = 0;
+    printf("\n");
+    printf("%s's Shopping Basket\n", c1.name);
+    printf("\n");
+    printf("ROW      ITEM NAME      COST\n");
+    for (i < basket.len ; ++i) {
+        printf("#%d     %s      %d\n", i, basket[i], basketpricevalues[i]);
+        total = total + basketpricevalues[i]; 
+    }
+    printf("\n");
+    printf("TOTAL: £%d\n", total);
+    getchar();
+    main();
 }
 
 void details () {
-
-
+    if (c1.name == NULL) {
+        printf("\n");
+        printf("What is your name?\n");
+        scanf("%s", c1.name);
+        printf("What is your postcode?\n");
+        scanf("%s", c1.postcode);
+        printf("How old are you?\n");
+        scanf("%d", c1.age);
+    }
+    else {
+        printf("\n");
+        printf("Found existing customer details:\n");
+        printf("Name: %s\n", c1.name);
+        printf("Postcode: %s\n", c1.postcode);
+        printf("Age: %d\n", c1.age);
+        printf("\n");
+        printf("Would you like to remove your details? (Y/N)\n");
+        char reply;
+        scanf("%c", reply);
+        (reply == "Y") ? free(c1) : 0;
+        printf("Your details have been removed.\n");
+    }
+    getchar();
+    main();
 }
 
 void error() {
     printf("Please enter a valid choice! \n");
     getchar();
+    main();
 }
 
 int main () {
@@ -40,7 +83,7 @@ int main () {
     printf("\n");
     printf("0 - BROWSE PRODUCTS\n");
     printf("1 - SHOPPING BASKET\n");
-    printf("2 - SET CUSTOMER DETAILS\n");
+    printf("2 - SET/GET CUSTOMER DETAILS\n");
     printf("\n");
     scanf("%s", input);
 
